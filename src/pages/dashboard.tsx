3c646459e94349d8fd50e2903cc3f5558197251b
Caio Lemec <caio.lemec@gmail.com>
2021-05-08 19:03:06 -0300

    criando dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { Header } from "../components/Header";
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react';
 import { Sidebar } from "../components/Sidebar";
+import React from "react";
 
 export default function Dashboard() {
     return (
@@ -9,6 +10,25 @@ export default function Dashboard() {
 
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius="8"
+                        //pb="4"
+                    >
+                        <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+                    </Box>
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius="8"
+                        //pb="4"
+                    >
+                        <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     );
