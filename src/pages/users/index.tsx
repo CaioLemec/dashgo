797b36fecdcacfbe17ab9493b674c4a1998803ef
Caio Lemec <caio.lemec@gmail.com>
2021-05-10 16:42:39 -0300

    Criando a lista de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
 import React from "react";
-import { RiAddLine } from "react-icons/ri";
+import { RiAddLine, RiPencilFill, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -21,13 +21,53 @@ export default function UserList () {
                             size="sm"
                             fontSize="sm"
                             colorScheme="pink"
-                            leftIcon={<Icon as={ RiAddLine } />}
+                            leftIcon={<Icon as={ RiAddLine } fontSize="20"/>}
                         >
                             Criar novo
                         </Button>
 
                     </Flex>
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" w="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th w="8">Botão</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                <Checkbox colorScheme="pink" /> 
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Caio Lemec</Text>
+                                        <Text fontSize="sm" color="gray.300">caiolemec@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    31 de Janeiro, 2021
+                                </Td>
+                                <Td>
+                                    <Button 
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={ RiPencilLine } fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
                 </Box>
+            
             </Flex>
         </Box>
     );
