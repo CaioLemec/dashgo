5a505341ddb03644f1193b3a38256c43724efb92
Caio Lemec <caio.lemec@gmail.com>
2021-05-19 20:38:29 -0300

    uncontrolled components

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,8 +1,16 @@
 import { Flex, Input, Icon, } from '@chakra-ui/react';
+import { useRef } from 'react';
 import { RiSearchLine } from 'react-icons/ri';
 
 
+
 export function SearchBox () {
+
+    const searchInputRef= useRef<HTMLInputElement>(null);
+
+    console.log(searchInputRef.current.value)
+    
+
     return (
         <Flex
         as="label"
@@ -25,8 +33,15 @@ export function SearchBox () {
             mr="4"
             placeholder="Buscar na plataforma"
             _placeholder={{color: 'gray.400'}}
+            ref={searchInputRef}
         />
         <Icon as={RiSearchLine} fontSize="20" />
     </Flex>  
     );
 }
+
+
+
+// const [search, setSearch] = useState('')
+// value={search}
+// onChange={event => setSearch(event.target.value)}
