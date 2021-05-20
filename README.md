f82b99e14ef3ad6238e29246d540890e189e8b7d
Caio Lemec <caio.lemec@gmail.com>
2021-05-19 21:13:20 -0300

    form de autenticaçao

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Usando chakra para criação de interfaces declarativas.
+
+https://chakra-ui.com/
+
+https://apexcharts.com/docs/installation/#
+
+lib forms: react-hook-form.com
--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,15 +1,7 @@
 import { Flex, Input, Icon, } from '@chakra-ui/react';
-import { useRef } from 'react';
 import { RiSearchLine } from 'react-icons/ri';
 
-
-
 export function SearchBox () {
-
-    const searchInputRef= useRef<HTMLInputElement>(null);
-
-    console.log(searchInputRef.current.value)
-    
 
     return (
         <Flex
@@ -33,7 +25,6 @@ export function SearchBox () {
             mr="4"
             placeholder="Buscar na plataforma"
             _placeholder={{color: 'gray.400'}}
-            ref={searchInputRef}
         />
         <Icon as={RiSearchLine} fontSize="20" />
     </Flex>  
