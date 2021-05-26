67b25f8c482a25e38a8d6e0e4833ff7c07b41c98
Caio Lemec <caio.lemec@gmail.com>
2021-05-25 21:17:15 -0300

    Listando usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,20 @@ export default function UserList () {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response = await fetch ('http://localhost:3000/api/users');
         const data = await response.json();
-        return data;
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            };
+        });
+
+        return users;
     })
 
     const isWideVersion = useBreakpointValue({
@@ -64,18 +77,22 @@ export default function UserList () {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            <Tr>
-                                <Td px={["4","4","6"]}>
-                                <Checkbox colorScheme="pink" /> 
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Caio Lemec</Text>
-                                        <Text fontSize="sm" color="gray.300">caiolemec@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                    { isWideVersion && <Td>29 de janeiro, 2021</Td> }
-                            </Tr>
+                            {data.map(user => {
+                                return (
+                                    <Tr key={user.id}>
+                                        <Td px={["4","4","6"]}>
+                                            <Checkbox colorScheme="pink" /> 
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">{user.name}</Text>
+                                                <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                            </Box>
+                                        </Td>
+                                        { isWideVersion && <Td>{user.createdAt}</Td> }
+                                    </Tr>
+                                )
+                            })}
                         </Tbody>
                     </Table>
                     <Pagination />
